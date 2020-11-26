74c4a344047d3daec5d76c8c8478fcbed956f374
katokiti <katokiti6101@gmail.com>
2020-11-26 16:29:21 +0900

    加藤日記追加

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from diaries.DiarySample import DiarySample
 from diaries.Ohashi import Ohashi
 from diaries.FukumotoDiaryNew import FukumotoDiaryNew
 from diaries.AminoDiary import AminoDiary
+from diaries.KatokitiDiary import KatokitiDiary
 
-diaries = [DiarySample(), Ohashi(),FukumotoDiaryNew(), AminoDiary(), ]
+diaries = [DiarySample(), Ohashi(),FukumotoDiaryNew(), AminoDiary(), Katokiti()]
 
 for d in diaries:
     print("---------------------------------")
